fc7de553e831da5b62ab665495c95e923c47e6ee
Juan Anaya <juan.anayar@udea.edu.co>
2022-06-17 03:43:00 -0500

    Problema terminado

--- Lab1_Problema12_AnayaJuan/main.cpp
+++ Lab1_Problema12_AnayaJuan/main.cpp
@@ -1,9 +1,54 @@
+/*
+                          INFORMATICA II
+Grupo 05
+Juan Sebastian Anaya Regino
+
+Problema 12. Escriba un programa que calcula el máximo factor primo de un número.
+Ej: Si se recibe 33 el programa debe imprimir 11.
+Nota: la salida del programa debe ser: El mayor factor primo de 33 es: 11.
+
+*/
+
 #include <iostream>
 
 using namespace std;
 
+bool esPrimo(int numero);
+
 int main()
 {
-    cout << "Hello World!" << endl;
+    int num, max=0;
+
+    /* Se imprime en pantalla la funcion del programa */
+    cout << "Este programa recibe un numero entero y calcula su maximo factor primo.\n\n";
+
+    /* Se le pide al usuario que ingrese un numero */
+    cout << "Ingrese un numero: ";
+    cin >> num;
+
+    /* Se calcula el maximo factor primo de num */
+    for(int i=2; i<num; ++i){
+        if(esPrimo(i)){
+            for(int j=1; j<num/2; ++j){
+                if(i*j == num && i>max){
+                    max = i;
+                }
+            }
+        }
+    }
+
+    /* Se imprime en pantalla el maximo factor primo de num */
+    cout << "\nEl mayor factor primo de " << num << " es: " << max << endl;
+
     return 0;
+}
+
+bool esPrimo(int numero){
+    for(int i=2; i<numero; i++){
+        if(numero%i==0){
+            return false;
+        }
+    }
+
+    return true;
 }
--- Lab1_Problema14_AnayaJuan/main.cpp
+++ Lab1_Problema14_AnayaJuan/main.cpp
@@ -1,9 +1,61 @@
+/*
+                          INFORMATICA II
+Grupo 05
+Juan Sebastian Anaya Regino
+
+Problema 14. Un número palíndromo es igual de derecha a izquierda y de izquierda a derecha,
+Ej: 969. escriba un programa que calcule el número palíndromo más grande que se puede obtener
+como una multiplicación de números de 3 dígitos.
+Ej: una de las posibles respuestas es: 143*777=111111.
+Nota: la salida del programa debe ser: 143*777=111111
+
+*/
+
 #include <iostream>
 
 using namespace std;
 
+bool esPalindromo(int num);
+
 int main()
 {
-    cout << "Hello World!" << endl;
+    int mayor = 0, numA, numB;
+
+    /* Se imprime en pantalla la funcion del programa */
+    cout << "Este programa calcula el numero palindromo mas grande que se puede "
+            "obtener como una multiplicacion de numeros de 3 digitos.\n\n";
+
+    /* Se calcula mayor producto palindromo entre dos numeros de tres cifras */
+    for(int i=100; i<999; ++i){
+        for(int j=100; j<999; ++j){
+            if(esPalindromo(i*j) && (i*j)>mayor){
+                numA = i;
+                numB = j;
+                mayor = i*j;
+            }
+        }
+    }
+
+    /* Se eimprime en pantalla el resultado */
+    cout << numA << "*" << numB << "=" << mayor << endl;
+
     return 0;
+}
+
+
+bool esPalindromo(int num)
+{
+    int numReverse = 0, aux = num;
+
+    do{
+        numReverse *= 10;
+        numReverse += aux % 10;
+        aux /= 10;
+    }while(aux>0);
+
+    if(numReverse != num){
+        return false;
+    }
+
+    return true;
 }
